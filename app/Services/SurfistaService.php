5add09137663ba8953e274ea9090454d4576d2dd
Agostinhoneto <agostneto6@gmail.com>
2023-11-17 10:29:43 -0300

    criando e refatorando services e repositories

--- app/Services/SurfistaService.php
+++ app/Services/SurfistaService.php
@@ -7,31 +7,31 @@ use App\Http\Repositories\SurfistaRepository;
 class SurfistaService
 {
 
-    protected $empresaRepository;
+    protected $surfistaRepository;
 
-    public function __construct(SurfistaRepository $empresaRepository)
+    public function __construct(SurfistaRepository $surfistaRepository)
     {
-        $this->empresaRepository = $empresaRepository;
+        $this->surfistaRepository = $surfistaRepository;
     }
 
     public function getAll($limit = 10)
     {
-        return $this->empresaRepository->getAll($limit);
+        return $this->surfistaRepository->getAll($limit);
     }
 
     public function getById($id)
     {
-        return $this->empresaRepository
+        return $this->surfistaRepository
             ->getById($id);
     }
 
-    public function register($user_id, $nome, $nome_social, $razao_social, $cnpj, $telefone, $email, $tipo_empresa_id, $natureza_empresa_id, $inscricao_empresa_id,$status,$usuario_cadastrante_id,$usuario_alterante_id)
+    public function register($numero,$nome,$pais)
     {
-        $result = $this->empresaRepository
-            ->save($user_id, $nome, $nome_social, $razao_social, $cnpj, $telefone, $email, $tipo_empresa_id, $natureza_empresa_id, $inscricao_empresa_id,$status,$usuario_cadastrante_id,$usuario_alterante_id);
+        $result = $this->surfistaRepository
+            ->save($numero,$nome,$pais);
         return $result;
     }
-    
+
     /*
     public function update($id, $nome, $nome_social, $razao_social, $cnpj, $telefone, $email, $tipo_empresa_id, $natureza_empresa_id, $inscricao_empresa_id,$status,$usuario_cadastrante_id,$usuario_alterante_id)
     {
